2e3c5dce8b0b8d01024cb70b3520c7a6ba03041c
Kartik <kartikprajapati987@gmail.com>
2021-05-09 16:23:17 +0530

    Add redis for state management

--- server/server.js
+++ server/server.js
@@ -1,249 +1,221 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const redis = require('redis');
+
+const client = redis.createClient("redis://redis:6379");
+
+client.on('connect', function () {
+  console.log('connected to redis');
+});
 
 const app = express();
 
 const server = http.createServer(app);
 
-//initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
 const clients = {};
 const games = {};
 const mapShapeToClientId = {};
+const onlineQueue = new Set();
 let updates = null;
 
 wss.on('connection', (ws) => {
-    //connection is up
-    ws.on('message', (message) => {
-        console.log('Received from client : ', message);
-        const response = JSON.parse(message);
-        if (response.type === 'create') {
-            const clientId = response.clientId;
-            const gameId = uuid();
-
-            games[gameId] = {
-                "id": gameId,
-                "clients": [{ clientId, shape: "cross" }],
-                "playerTurn": clientId
-            }
-
-            const payLoad = {
-                "type": "create",
-                "game": games[gameId]
-            }
-
-            ws.send(JSON.stringify(payLoad));
+  ws.on('close', function close() {
+    onlineQueue.delete(ws.clientId);
+    console.log('on close', ws.clientId);
+  });
+  ws.on('message', (message) => {
+    const response = JSON.parse(message);
+    if (response.type === 'create') {
+      const clientId = response.clientId;
+      const gameId = uuid();
+      const gameData = {
+        "id": gameId,
+        "clients": [{ clientId, shape: "cross" }],
+        "playerTurn": clientId
+      };
+      client.set(gameId, JSON.stringify(gameData), (err, reply) => {
+        const payLoad = {
+          "type": "create",
+          "game": gameData
         }
-        if (response.type === 'join') {
-            const clientId = response.clientId;
-            const gameId = response.gameId;
-            const game = games[gameId];
-            if (game.clients.length >= 3) {
-                console.log("Sorry! Maximum players reached!");
-                return;
-            }
-            const shape = { "0": "cross", "1": "circle", "2": "triangle" }[game.clients.length];
-            game.clients.push({
-                "clientId": clientId,
-                "shape": shape
-            });
-            mapShapeToClientId[shape] = clientId;
+        ws.send(JSON.stringify(payLoad));  
+      });
+    }
+    if (response.type === 'join') {
+      const clientId = response.clientId;
+      const gameId = response.gameId;
+      client.get(gameId, (err, reply) => {
+        console.log(reply)
+        const game = JSON.parse(reply);
+        if (game.clients.length >= 3) {
+          return;
+        }
+        const shape = { "0": "cross", "1": "circle", "2": "triangle" }[game.clients.length];
+        game.clients.push({
+          "clientId": clientId,
+          "shape": shape
+        });
+        client.set(gameId, JSON.stringify(game), (err, reply) => {
+          mapShapeToClientId[shape] = clientId;
+          const payLoad = {
+            "type": "join",
+            "game": game
+          }
+          game.clients.forEach(c => {
+            clients[c.clientId].ws.send(JSON.stringify(payLoad));
+          })    
+        });
+      });
+    }
 
-            // if(game.clients.length === 3){
-            //     updates = setInterval(updateGameState,500);
-            // }
+    if (response.type === 'play') {
+      const clientId = response.clientId;
+      const gameId = response.gameId;
+      const box = response.box;
+      client.get(gameId, (err, reply) => {
+        const game = JSON.parse(reply);
+        const shape = response.shape;
 
-            const payLoad = {
-                "type": "join",
-                "game": game
-            }
+        let turnIndex = 0;
+        game.clients.forEach((c, idx) => {
+          if (c.clientId === game.playerTurn) turnIndex = idx;
+        });
 
+        game.playerTurn = game.clients[(turnIndex + 1) % 3].clientId;
+        let state = game.state;
+        if (!state)
+          state = {};
+        state[box] = shape
+        game.state = state;
+
+        const winner = calculateWinner(game.state);
+
+        client.set(gameId, JSON.stringify(game), (err, reply) => {
+          sendUpdateToPlayers(game);
+          if (winner !== "No winner") {
             game.clients.forEach(c => {
+              if (c.shape === winner) {
+                const payLoad = {
+                  type: "winner",
+                }
                 clients[c.clientId].ws.send(JSON.stringify(payLoad));
-            })
-        }
-        if (response.type === 'play') {
-            const clientId = response.clientId;
-            const gameId = response.gameId;
-            const box = response.box;
-            const game = games[gameId];
-            const shape = response.shape;
-
-            let turnIndex = 0;
-            game.clients.forEach((c, idx) => {
-                if (c.clientId === game.playerTurn) turnIndex = idx;
-            });
-            console.log('TI', turnIndex);
-
-            game.playerTurn = game.clients[(turnIndex + 1) % 3].clientId;
-            console.log('PT', game.playerTurn);
-            let state = game.state;
-            if (!state)
-                state = {};
-            state[box] = shape
-            game.state = state;
-
-            const winner = calculateWinner(game.state);
-            console.log('after calc winner', winner);
-            sendUpdateToPlayers(game);
-            if (winner !== "No winner") {
-                game.clients.forEach(c => {
-                    if (c.shape === winner) {
-                        const payLoad = {
-                            type: "winner",
-                        }
-                        clients[c.clientId].ws.send(JSON.stringify(payLoad));
-                    }
-                    else {
-                        const payLoad = {
-                            type: "loser",
-                        }
-                        clients[c.clientId].ws.send(JSON.stringify(payLoad));
-                    }
-                })
-            }
-        }
-        if (response.type === 'winner') {
-            clearInterval(updates);
-            response.game.clients.forEach(c => {
-                if (c.clientId === mapShapeToClientId[response.winner]) {
-                    const payLoad = {
-                        type: "winner",
-                    }
-                    clients[c.clientId].ws.send(JSON.stringify(payLoad));
+              }
+              else {
+                const payLoad = {
+                  type: "loser",
                 }
-                else {
-                    const payLoad = {
-                        type: "loser",
-                    }
-                    clients[c.clientId].ws.send(JSON.stringify(payLoad));
-                }
+                clients[c.clientId].ws.send(JSON.stringify(payLoad));
+              }
             })
-            delete games[response.game.id];
-        }
-    });
-    // generate a new clientId
-    const clientId = uuid();
-    clients[clientId] = {
-        "ws": ws,
+            console.log(gameId);
+            client.del(gameId, redis.print);
+          }  
+        });
+      })
     }
+    if (response.type === 'playOnline') {
+      onlineQueue.add(response.clientId);
+      if (onlineQueue.size >= 3) {
+        var it = onlineQueue.values();
+        const user1 = it.next().value;
+        onlineQueue.delete(user1);
+        const user2 = it.next().value;
+        onlineQueue.delete(user2);
+        const user3 = it.next().value;
+        onlineQueue.delete(user3);
 
-    const payLoad = {
-        "type": "connect",
-        "clientId": clientId,
+        const gameId = uuid();
+
+        const gameData = {
+          "id": gameId,
+          "clients": [
+            { clientId: user1, shape: "cross" },
+            { clientId: user2, shape: "circle" },
+            { clientId: user3, shape: "triangle" },
+          ],
+          "playerTurn": user1
+        };
+
+        client.set(gameId, JSON.stringify(gameData), (err, reply) => {
+          const payLoad = {
+            "type": "join",
+            "game": gameData
+          }
+  
+          gameData.clients.forEach(c => {
+            clients[c.clientId].ws.send(JSON.stringify(payLoad));
+          })  
+        });
+      }
     }
+  });
+  const clientId = uuid();
+  clients[clientId] = {
+    "ws": ws,
+  }
+  ws.clientId = clientId;
+  console.log('on connection', ws.clientId);
+  const payLoad = {
+    "type": "connect",
+    "clientId": clientId,
+  }
 
-    ws.send(JSON.stringify(payLoad));
+  ws.send(JSON.stringify(payLoad));
 });
 
 server.listen(8080, () => {
-    console.log("Listening on port : 8080");
+  console.log("Listening on port : 8080");
 });
 
 const uuid = () => {
-    return Math.floor(Math.random() * 100) + "-" + Math.floor(Math.random() * 100) + "-" + Math.floor(Math.random() * 100);
-}
-
-const updateGameState = () => {
-    for (const g of Object.keys(games)) {
-        const game = games[g];
-        const payLoad = {
-            "type": "update",
-            "game": game,
-        }
-        game.clients.forEach(c => {
-            clients[c.clientId].ws.send(JSON.stringify(payLoad));
-        })
-        console.log('update PT', game.playerTurn);
-    }
-    //setTimeout(updateGameState,500);
+  return Math.floor(Math.random() * 100) + "-" + Math.floor(Math.random() * 100) + "-" + Math.floor(Math.random() * 100);
 }
 
 const sendUpdateToPlayers = (game) => {
-    const payLoad = {
-        "type": "update",
-        "game": game,
-    }
+  const payLoad = {
+    "type": "update",
+    "game": game,
+  }
 
-    game.clients.forEach(c => {
-        clients[c.clientId].ws.send(JSON.stringify(payLoad));
-    })
-    //setTimeout(updateGameState,500);
+  game.clients.forEach(c => {
+    clients[c.clientId].ws.send(JSON.stringify(payLoad));
+  })
 }
 
 const calculateWinner = (state) => {
-    console.log(state);
-    // //rows
-    // if (state[1] === state[2] && state[2] === state[3]) return state[1];
-    // if (state[2] === state[3] && state[3] === state[4]) return state[2];
-
-    // if (state[5] === state[6] && state[6] === state[7]) return state[5];
-    // if (state[6] === state[7] && state[7] === state[8]) return state[6];
-
-    // if (state[9] === state[10] && state[10] === state[11]) return state[9];
-    // if (state[10] === state[11] && state[11] === state[12]) return state[10];
-
-    // if (state[13] === state[14] && state[14] === state[15]) return state[13];
-    // if (state[14] === state[15] && state[15] === state[16]) return state[14];
-    // //colums
-    // if (state[1] === state[5] && state[5] === state[9]) return state[1];
-    // if (state[5] === state[9] && state[9] === state[13]) return state[5];
-
-    // if (state[2] === state[6] && state[6] === state[10]) return state[6];
-    // if (state[6] === state[10] && state[10] === state[14]) return state[6];
-
-    // if (state[3] === state[7] && state[7] === state[11]) return state[11];
-    // if (state[7] === state[11] && state[11] === state[15]) return state[11];
-
-    // if (state[4] === state[8] && state[8] === state[12]) return state[12];
-    // if (state[8] === state[12] && state[12] === state[16]) return state[12];
-    // //diagonals
-    // if (state[1] === state[6] && state[6] === state[11]) return state[6];
-    // if (state[3] === state[6] && state[6] === state[9]) return state[6];
-
-    // if (state[2] === state[7] && state[7] === state[12]) return state[7];
-    // if (state[4] === state[7] && state[7] === state[10]) return state[7];
-
-    // if (state[5] === state[10] && state[10] === state[15]) return state[10];
-    // if (state[7] === state[10] && state[10] === state[13]) return state[10];
-
-    // if (state[6] === state[11] && state[11] === state[16]) return state[11];
-    // if (state[8] === state[11] && state[11] === state[14]) return state[11];
-
-    let mat = [];
-    for (let i = 0; i < 4; i++) {
-        mat.push([]);
-        for (let j = 0; j < 4; j++) {
-            mat[i].push(-1);
-        }
+  let mat = [];
+  for (let i = 0; i < 4; i++) {
+    mat.push([]);
+    for (let j = 0; j < 4; j++) {
+      mat[i].push(-1);
     }
-    console.log('mat init', mat);
-    Object.keys(state).forEach(key => {
-        --key;
-        console.log(key);
-        key = Number(key);
-        console.log(mat[Number(Math.floor(key/4))][key%4]);
-        console.log(mat[Number(Math.floor(key/4))]);
-        mat[Number(Math.floor(key/4))][key%4] = state[key+1];
-        console.log(mat);
-    })
+  }
+  Object.keys(state).forEach(key => {
+    --key;
+    key = Number(key);
+    mat[Number(Math.floor(key / 4))][key % 4] = state[key + 1];
+  })
 
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (mat[i][j] === -1) continue;
-            if (j !== 0 && j+1 < 4) {
-                if (mat[i][j-1] === mat[i][j] && mat[i][j+1] === mat[i][j]) return mat[i][j];
-            }
-            if (i !== 0 && i+1 < 4) {
-                if (mat[i-1][j] === mat[i][j] && mat[i+1][j] === mat[i][j]) return mat[i][j];
-            }
-            if (i !== 0 && i+1 < 4 && j !== 0 && j+1 < 4) {
-                if (mat[i-1][j-1] === mat[i][j] && mat[i+1][j+1] === mat[i][j]) return mat[i][j];
-                if (mat[i-1][j+1] === mat[i][j] && mat[i+1][j-1] === mat[i][j]) return mat[i][j];
-            }
-        }
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (mat[i][j] === -1) continue;
+      if (j !== 0 && j + 1 < 4) {
+        if (mat[i][j - 1] === mat[i][j] && mat[i][j + 1] === mat[i][j]) return mat[i][j];
+      }
+      if (i !== 0 && i + 1 < 4) {
+        if (mat[i - 1][j] === mat[i][j] && mat[i + 1][j] === mat[i][j]) return mat[i][j];
+      }
+      if (i !== 0 && i + 1 < 4 && j !== 0 && j + 1 < 4) {
+        if (mat[i - 1][j - 1] === mat[i][j] && mat[i + 1][j + 1] === mat[i][j]) return mat[i][j];
+        if (mat[i - 1][j + 1] === mat[i][j] && mat[i + 1][j - 1] === mat[i][j]) return mat[i][j];
+      }
     }
-    return "No winner";
+  }
+  return "No winner";
 }
+
+
+var exports = module.exports;
